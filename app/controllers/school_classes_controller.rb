046b4df9787c1eb0c09ac92b34deec98d220186d
Sean Altemose <sean@seanaltemose.com>
2018-11-25 23:01:09 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,6 +11,12 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.find(params[:id])
   end
 
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save 
+    redirect_to school_class_path(@school_class)
+  end 
+
   def show
     @school_class = SchoolClass.find(params[:id])
   end
